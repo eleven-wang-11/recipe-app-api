c8fc64164c4e6d40e05250744ec8fe8ecb8b62cb
wangwei1113 <elevenwang1003@gmail.com>
2022-09-22 15:07:10 +0800

    Added recipe model.

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -40,6 +41,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    """this is a AbstarctBaseUser field?"""
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe object."""
+    """ForeignKey allows you to set a relationship bew this model and another model
+        settings.AUTH_USER_MODEL,this is the model you want to link;
+        on_delete=models.CASCADE delete all recipe object if the user is deleted from user model
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    "allow you to display it in django otherwise it display the id"
+    def __str__(self):
+        return self.title
+
